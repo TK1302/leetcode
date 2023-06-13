0ed5abdd8baa4c09b736f3ca27c95c0a7672b9cb
Tulsi <126496600+TK1302@users.noreply.github.com>
2023-06-14 02:44:55 +0530

    Time: 44 ms (80.06%), Space: 14.2 MB (83.31%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def twoSum(self, nums, target):
+        # Create a hash table to store the elements and their indices
+        hash_table = {}
+        
+        # Iterate through the array
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            
+            # Check if the complement exists in the hash table
+            if complement in hash_table:
+                return [hash_table[complement], i]
+            
+            # Add the current element to the hash table
+            hash_table[nums[i]] = i
+        
+        # If no solution is found, return an empty array
+        return []
